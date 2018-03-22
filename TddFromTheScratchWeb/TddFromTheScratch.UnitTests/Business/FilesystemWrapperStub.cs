7446ce235ed5f13c0a79d3351c105da88a0390a4
luisraed@gmail.com <luisraed@gmail.com>
2018-03-22 13:59:48 -0300

    Unit Test using property injection

---
+++ TddFromTheScratchWeb/TddFromTheScratch.UnitTests/Business/FilesystemWrapperStub.cs
@@ -0,0 +1,27 @@
+﻿using TddFromTheScratch.Business;
+
+namespace TddFromTheScratch.UnitTests.Business
+{
+    public class FilesystemWrapperStub : IFilesystemWrapper
+    {
+        public bool Exists { get; set; }
+        public string Path { get; set; }
+        public string[] Lines { get; set; }
+
+        public bool FileExists(string path)
+           {
+            return Exists;
+        }
+
+        public string PathCombine(string fileName)
+        {
+            return Path;
+        }
+
+        public string[] ReadAlllines(string path)
+        {
+            return Lines;
+        }
+    }
+
+}
